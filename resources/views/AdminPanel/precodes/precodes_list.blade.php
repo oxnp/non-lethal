7c634f58c997b980309d73e6af6869177b607b25
mokrel <mokrelmokrel@gmail.com>
2019-11-06 16:12:08 +0200

    Pre-codes list

--- resources/views/AdminPanel/precodes/precodes_list.blade.php
+++ resources/views/AdminPanel/precodes/precodes_list.blade.php
@@ -2,62 +2,59 @@
 @extends('layouts.app-admin-leftsidebar')
 @extends('layouts.app-admin-header')
 @section('content')
-    <table width="1300px">
-        <tr>
-            <td>ID</td>
-            <td>Pre-Activation code</td>
-            <td>Product code</td>
-            <td>Product name</td>
-            <td>Type</td>
-            <td>License system</td>
-            <td>Details</td>
-            <td>Reference</td>
-        </tr>
-    @foreach($precodes as $precode)
-            <tr>
-                <td>{{$precode->id}}</td>
-                <td>{{substr(chunk_split($precode->precode,5,'-'),0,-1)}} {{$precode->used}}</td>
-                <td>{{$precode->code}}</td>
-                <td>{{$precode->name}}</td>
-                <td>
-                    @php
-                    $licenseTypes = array(
-                        0 => 'feature',
-                        1 => 'full',
-                        2 => 'upgrade',
-                        3 => 'temp'
-                    );
-                    echo ucfirst($licenseTypes[intval($precode->type)]);
-                    @endphp
-                </td>
-                <td>
-                @php
-                    $license = array(
-                        1 => 'NLA Licensing',
-                        2 => 'PACE iLok'
-                    );
-                    echo ucfirst($license[intval($precode->licsystem)]);
-                @endphp
-                </td>
-                <td>
-                    @php $itemData = json_decode($precode->data);  @endphp
-                    @php
-                    switch ($precode->type) {
-                        case 0:
-                             echo 'Feature Name: ' .$itemData->feature_name;
-                            break;
-                        case 3:
-                            echo 'Duration: ' .$itemData->temp_days. ' days';
-                            break;
 
-                        default:
-                    }
-                    @endphp
-                </td>
-                <td>{{$precode->reference}}</td>
-            </tr>
-    @endforeach
-    </table>
+    <div class="list">
+        <div class="list_head">
+            <div class="col-md-1">ID</div>
+            <div class="col-md-2">Pre-Activation code</div>
+            <div class="col-md-1">Product code</div>
+            <div class="col-md-2">Product name</div>
+            <div class="col-md-1">Type</div>
+            <div class="col-md-1">License system</div>
+            <div class="col-md-2">Details</div>
+            <div class="col-md-2">Reference</div>
+        </div>
+        <div class="list_body">
+            @foreach($precodes as $precode)
+                <div class="item">
+                    <div class="col-md-1 idcol">{{$precode->id}}</div>
+                    <div class="col-md-2">{{substr(chunk_split($precode->precode,5,'-'),0,-1)}} {{$precode->used}}</div>
+                    <div class="col-md-1">{{$precode->code}}</div>
+                    <div class="col-md-2">{{$precode->name}}</div>
+                    <div class="col-md-1"> @php
+                            $licenseTypes = array(
+                                0 => 'feature',
+                                1 => 'full',
+                                2 => 'upgrade',
+                                3 => 'temp'
+                            );
+                            echo ucfirst($licenseTypes[intval($precode->type)]);
+                        @endphp</div>
+                    <div class="col-md-1">@php
+                            $license = array(
+                                1 => 'NLA Licensing',
+                                2 => 'PACE iLok'
+                            );
+                            echo ucfirst($license[intval($precode->licsystem)]);
+                        @endphp</div>
+                    <div class="col-md-2">@php $itemData = json_decode($precode->data);  @endphp
+                        @php
+                            switch ($precode->type) {
+                                case 0:
+                                     echo 'Feature Name: ' .$itemData->feature_name;
+                                    break;
+                                case 3:
+                                    echo 'Duration: ' .$itemData->temp_days. ' days';
+                                    break;
+
+                                default:
+                            }
+                        @endphp</div>
+                    <div class="col-md-2">{{$precode->reference}}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
     {{$precodes->links()}}
 @stop
 
