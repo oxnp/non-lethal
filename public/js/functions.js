e779faaa2cefe7a275d79e7d4301a77b8c630c24
mokrel <mokrelmokrel@gmail.com>
2019-11-25 10:46:33 +0200

    Homepage, add license admin button

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-    $('.slider').owlCarousel({
+    $('.slider, .user_stories').owlCarousel({
         items: 1,
         loop: true,
+        nav: true,
+        dots: false,
+        navText: ["<span></span><svg width=\"10\" height=\"17\" viewBox=\"0 0 10 17\">\n" +
+        "<use xlink:href=\"#arrow\" x=\"0\" y=\"0\" />\n" +
+        "</svg>","<span></span><svg width=\"10\" height=\"17\" viewBox=\"0 0 10 17\">\n" +
+        "<use xlink:href=\"#arrow\" x=\"0\" y=\"0\" />\n" +
+        "</svg>"]
+    });
+    $('.news_slider').owlCarousel({
+        items: 3,
+        loop:true,
         nav: true,
         dots: false,
         navText: ["<span></span><svg width=\"10\" height=\"17\" viewBox=\"0 0 10 17\">\n" +
