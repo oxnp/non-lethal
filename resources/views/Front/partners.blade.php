8ef471e68725da2b0bedaaaeee735cfd7026cda8
mokrel <mokrelmokrel@gmail.com>
2019-11-26 15:20:05 +0200

    Partners page

---
+++ resources/views/Front/partners.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.app-front')
+@section('app-front-content')
+    <section class="intro partners">
+        <h1>Partners</h1>
+    </section>
+    <section id="partners">
+        <div class="container">
+            <div class="pretext">
+                Non-Lethal Applications have a network of distributors and dealer around the world who can offer pre-sales and after-sales advice and support. If you are looking to buy one of our products, then check out the list below or contact us to be directed to the most appropriate person for your enquiry.
+            </div>
+            <div class="row">
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">United Kingdom</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_uk.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>HHB Communications Ltd</p>
+                        <p>73-75 Scrubs Lane<br />
+                            London, NW10 6QU</p>
+                        <p>Phone (London W1 Office): +44 (0)20 7025 6020</p>
+                        <p>Phone (NW10 HQ): +44 (0)20 8962 5000<br />
+                        Email: sales (at)hhb.co.uk<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.hhb.co.uk">www.hhb.co.uk</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">United States</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_us.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>RSPE Audio Solutions</p>
+                        <p>4130 Cahuenga Blvd., Suite #210<br />
+                            Universal City, CA 91602</p>
+                        <p>Phone: (818) 509-5876 / Toll Free: (855) 632-1032</p>
+                        <p>Email: websales (at) rspe.com<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.rspeaudio.com">www.rspeaudio.com</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">Germany</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_de.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>SMM GmbH</p>
+                        <p>Bleicherfleck 3<br />
+                            85737 Ismaning</p>
+                        <p>Tel. +49 (0)89-992 88 992<br />
+                        Email: m.werner (at) smm-online.de<br />
+                            Web: <a target="_blank" rel="nofollow" href="smm-online.de">smm-online.de</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">Japan</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_jp.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>Rock On Pro</p>
+                        <p>Jinnan Watanabe Building 2F<br />
+                            1-4-8 Jinnan Shibuya-Ku, Tokyo</p>
+                        <p>Tel. 03-3477-1776<br />
+                        Email: ymaeda (at) miroc.co.jp<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.miroc.co.jp">www.miroc.co.jp</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">The Netherlands</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_nd.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>Maartens Sound and Vision</p>
+                        <p>Businesspark Friesland West 37<br />
+                            8447 SL Heerenveen</p>
+                        <p>Tel. +31-(0)513-629120<br />
+                        Email: aernout (at) msv.nl<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.msv.nl">www.msv.nl</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">Poland</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_pl.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>Mitkom</p>
+                        <p>ul. Liliowa 27W<br />
+                            04-953 Warszawa</p>
+                        <p>Tel. 509 902 004<br />
+                        Email: artur.mitrosz (at) mitkom.pl<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.mitkom.pl">www.mitkom.pl</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">Italy</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_it.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>Bagnetti S.r.l.</p>
+                        <p>Piazza Gentile da Fabriano 4<br />
+                            00196 - Roma</p>
+                        <p>Tel.: +39-06-3232773 - Ext. 12<br />
+                        Email: paolo.gambarelli (at) bagnetti.com<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.bagnetti.com">www.bagnetti.com</a></p>
+                    </div>
+                </div>
+                <div class="partner col-md-6">
+                    <div class="row align-items-center">
+                        <div class="title col-md">Australia</div>
+                        <div class="col-md text-center">
+                            <img src="/images/partners/partner_aus.png">
+                        </div>
+                    </div>
+                    <div class="desc">
+                        <p>Musiclab</p>
+                        <p>Level 2, 34 Campbell St<br />
+                            Bowen Hills, Q4006</p>
+                        <p>Tel. +61 - 07 3270 1111<br />
+                        Email: chris (at) musiclab.com.au<br />
+                            Web: <a target="_blank" rel="nofollow" href="www.musiclab.com.au">www.musiclab.com.au</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
