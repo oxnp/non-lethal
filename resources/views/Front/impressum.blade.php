7ca822d955aaa788318567f1f00b74f1807ce603
mokrel <mokrelmokrel@gmail.com>
2019-11-26 15:31:13 +0200

    Impressum page

---
+++ resources/views/Front/impressum.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app-front')
+@section('app-front-content')
+    <section class="intro impressum">
+        <h1>Impressum</h1>
+    </section>
+    <section id="impressum">
+        <div class="container">
+            <div class="desc">
+                <p>The following information (Impressum) is required under German law.</p>
+                <p><strong>Founded by Florian (Flo) Loferer, Non-Lethal Applications is a software development company based in Southern Germany.</strong></p>
+                <p>Florian Loferer<br />
+                Loisachstrasse 1<br />
+                    83101 Rohrdorf</p>
+                <p>Telefon +49 8031 7967750<br />
+                    <a href="mailto:info@non-lethal-applications.com">info@non-lethal-applications.com</a><br />
+                    International VAT ID DE263743606</p>
+                <p>Our order process is conducted by our online reseller Paddle.com. Paddle.com is the Merchant of Record for all our orders. Paddle provides all customer service inquiries and ha ndles returns.</p>
+                <p><a href="/disclaimer">Disclaimer / Privacy Policy</a></p>
+            </div>
+        </div>
+    </section>
+@endsection
