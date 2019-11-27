7012819641d6a470706082806bd699e2f49dffd2
mokrel <mokrelmokrel@gmail.com>
2019-11-27 08:35:36 +0200

    Company page start

---
+++ resources/views/Front/company.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app-front')
+@section('app-front-content')
+    <section class="intro company">
+        <h1>Company</h1>
+    </section>
+    <section id="company">
+        <div class="container">
+
+        </div>
+    </section>
+@endsection
