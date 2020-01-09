162e784cc8f6945f0fc1771315792cd0c4fd104c
mokrel <mokrelmokrel@gmail.com>
2020-01-09 14:35:43 +0200

    Emails templates list and inner

--- resources/views/AdminPanel/emailstemplates/email_templates_list.blade.php
+++ resources/views/AdminPanel/emailstemplates/email_templates_list.blade.php
@@ -1,1 +1,20 @@
- 
+@extends('layouts.app-admin')
+@extends('layouts.app-admin-leftsidebar')
+@section('content')
+    <div class="list">
+        <div class="list_head">
+            <div class="col-md-1">ID</div>
+            <div class="col-md-6">Name</div>
+            <div class="col-md-5">Alias name</div>
+        </div>
+        <div class="list_body">
+            @foreach($templates as $template)
+                <div class="item">
+                    <div class="col-md-1 idcol">{{$template->id}}</div>
+                    <div class="col-md-6"><a href="{{route('emails-templates.show',$template->id)}}">{{$template->name}}</a></div>
+                    <div class="col-md-5">{{$template->alias_name}}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@stop
